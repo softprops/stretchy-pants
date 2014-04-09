71ef8973def723a562973a4cb84c18a81f19948d
softprops <d.tangren@gmail.com>
2014-04-09 08:36:45 -0400

    flush out source

--- src/main/scala/Source.scala
+++ src/main/scala/Source.scala
@@ -1,9 +1,20 @@
 package stretchypants
 
-sealed trait Source
+import org.json4s.{ JArray, JBool, JString, JValue }
+import org.json4s.JsonDSL._
+
+sealed trait Source {
+  def asJson: JValue
+}
 
 object Source {
-  case object None extends Source
-  case class Include(include: List[String]) extends Source
-  case class Mixed(include: List[String], exclude: List[String]) extends Source
+  case object None extends Source {
+    def asJson = JBool(false)
+  }
+  case class Include(include: List[String]) extends Source {
+    def asJson = JArray(include.map(JString(_)))
+  }
+  case class Mixed(include: List[String], exclude: List[String]) extends Source {
+    def asJson = ("include" -> include) ~ ("exclude" -> exclude)
+  }
 }
